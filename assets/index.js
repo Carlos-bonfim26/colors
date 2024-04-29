d990f0f75e8451ac8afe1f2e7b87021d643c9d2a
carlos <carlosbonfim722@gmail.com>
2024-04-29 14:30:54 -0300

    version-final

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+function trocaCor(){
+    let hex_numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+    let hexcode = '';
+
+    for(let i = 0; i < 6; i++){
+        let random_index = Math.floor(Math.random() * hex_numeros.length);
+        hexcode += hex_numeros[random_index]
+    }
+    document.getElementById('codigo-hex').innerHTML = hexcode;
+    document.getElementsByTagName('body')[0].style.background = '#' + hexcode
+}
